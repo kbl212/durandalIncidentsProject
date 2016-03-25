f474779ec6733baf1161afaea133bf15b50a058a
kbl212 <kyle.lauritzen@gmail.com>
2016-03-25 00:36:12 -0600

    durandal added, mongoDB set up correctly

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(cors());
 
+///////////////
+//CONNECTIONS//
+///////////////
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
     console.log("listening on port ", port);
 })
+
+var mongoUri = "mongodb://localhost:27017/durandalIncidents";
+mongoose.connect(mongoUri, function(err) {
+    if (err) throw err;
+});
+
+mongoose.connection.once('open', function () {
+    console.log('connected to mongoDB at: ', mongoUri);
+});
